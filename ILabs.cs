3df0eeb2bf17fa6faceadd42d034a88864b9bd38
CyrodiilNerevarine <113711436+CyrodiilNerevarine@users.noreply.github.com>
2022-10-24 19:07:35 +0300

    Update ILabs.cs

--- ILabs.cs
+++ ILabs.cs
@@ -1,14 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Lab1
-{
-    
-    {
-        void Demo();
-        string Name();
-        string Description();
-        int Id();
-    }
-}
